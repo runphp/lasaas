a3524765f5934d92e402a7834b49eac4caa98290
runphp <runphp@qq.com>
2026-05-22 15:50:46 +0800

    feat(i18n): 添加英文和中文语言包及国际化配置

--- config/tenancy.php
+++ config/tenancy.php
@@ -17,9 +17,9 @@ return [
      * Only relevant if you're using the domain or subdomain identification middleware.
      */
     'central_domains' => [
+        'lasaas.ddev.site',
         '127.0.0.1',
         'localhost',
-        'lasaas.ddev.site',
     ],
 
     /**
