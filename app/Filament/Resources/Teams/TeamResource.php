887681fe2651f4ded2d13956274f6f88beb6887a
runphp <runphp@qq.com>
2026-06-16 09:24:53 +0800

    refactor: 更新资源的导航图标

--- app/Filament/Resources/Teams/TeamResource.php
+++ app/Filament/Resources/Teams/TeamResource.php
@@ -22,7 +22,7 @@ class TeamResource extends Resource
 {
     protected static ?string $model = Team::class;
 
-    protected static string|BackedEnum|null $navigationIcon = Heroicon::OutlinedRectangleStack;
+    protected static string|BackedEnum|null $navigationIcon = Heroicon::OutlinedUserGroup;
 
     protected static ?string $recordTitleAttribute = 'name';
 
--- app/Filament/Resources/Tenants/TenantResource.php
+++ app/Filament/Resources/Tenants/TenantResource.php
@@ -20,7 +20,7 @@ class TenantResource extends Resource
 {
     protected static ?string $model = Tenant::class;
 
-    protected static string|BackedEnum|null $navigationIcon = Heroicon::OutlinedRectangleStack;
+    protected static string|BackedEnum|null $navigationIcon = Heroicon::OutlinedBuildingOffice;
 
     protected static ?string $recordTitleAttribute = 'name';
 
